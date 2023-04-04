a24ccc4ba7a0ec9b91f2b13731977305a5538f25
Цикура Марія <117911005+mariiatsykura@users.noreply.github.com>
2023-04-05 01:44:03 +0300

    Add files via upload

---
+++ Task01/main.java
@@ -0,0 +1,15 @@
+/**
+    * Print command-line parameters.
+    */
+public class main {
+    
+    /**
+    * Program entry point.
+    * @param args command-line parameters list
+    */
+    public static void main(String[] args) {
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
